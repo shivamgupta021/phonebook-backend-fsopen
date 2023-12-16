3b59a49534b4b074cf916e6e29e530d74786f6b6
shivamgupta021 <shivam.gupta02210@gmail.com>
2023-12-16 19:13:19 +0530

    uibuild

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors")
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static("dist"))
 
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body)
